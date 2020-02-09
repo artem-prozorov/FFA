d0f764baceba3e42a8bc7231a8a96a09307f1789
Artem Prozorov <artem.g.prozorov@gmail.com>
2020-02-09 18:03:45 +0300

    Добавил докблок к контракту GameServiceInterface

--- app/Contracts/Game/GameServiceInterface.php
+++ app/Contracts/Game/GameServiceInterface.php
@@ -6,9 +6,31 @@ use App\Models\{Game, User};
 
 interface GameServiceInterface
 {
+    /**
+     * Creates a new Game with the specified Settings
+     *
+     * @access	public
+     * @param	SettingsInterface	$settings
+     * @return	Game
+     */
     public function createNewGame(SettingsInterface $settings): Game;
 
+    /**
+     * Checks if a user can apply for a game and registers the user
+     *
+     * @access	public
+     * @param	User	$user
+     * @param	Game	$game
+     * @return	void
+     */
     public function applyUserForGame(User $user, Game $game): void;
 
+    /**
+     * Starts the game
+     *
+     * @access	public
+     * @param	Game	$game
+     * @return	void
+     */
     public function startTheGame(Game $game): void;
 }
