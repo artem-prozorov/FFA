39fd066fbd85d560928a4ad5d59ece2afab82925
anattoliy90 <anattoliy90@gmail.com>
2020-02-09 20:02:25 +0300

    declared interfaces

--- app/Models/Artefact.php
+++ app/Models/Artefact.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 use App\Traits\HasOnePosition;
+use App\Contracts\TakeableInterface;
 
 class Artefact extends Model implements TakeableInterface
 {
--- app/Models/Player.php
+++ app/Models/Player.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 use App\Traits\HasOnePosition;
+use App\Contracts\AttackableInterface;
 
 class Player extends Model implements AttackableInterface
 {
