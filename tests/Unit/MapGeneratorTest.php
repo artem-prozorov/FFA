41ed054d3d62be61b2db6ef86332843438968795
Emil Aliev <eo.aliev@bizprofi.ru>
2020-02-09 23:45:54 +0300

    Fix map generator test

--- tests/Unit/MapGeneratorTest.php
+++ tests/Unit/MapGeneratorTest.php
@@ -2,18 +2,59 @@
 
 namespace Tests\Unit;
 
-use Tests\TestCase;
-use App\Services\Game\Settings;
+use App\Contracts\Game\SettingsInterface;
+use App\Contracts\Map\CoordinatesServiceInterface;
+use App\Models\Game;
+use App\Models\Map;
+use App\Models\Position;
 use App\Services\Map\MapGenerator;
+use Tests\TestCase;
 
 class MapGeneratorTest extends TestCase
 {
-    public function tesMaptGenerator()
+    public function testMapGenerator()
     {
-        $settings = new Settings();
-        $service = new MapGenerator();
+        $settings = new class () implements SettingsInterface {
+            protected $mapWidth = 0;
+            protected $mapHeight = 0;
+            protected $dificultyPercentage = 0;
 
-        $map = $service->create($settins);
+            public function _construct()
+            {
+                $this->mapWidth = rand(100, 1000);
+                $this->mapHeight = rand(100, 1000);
+                $this->dificultyPercentage = rand(1, 100);
+            }
+
+            public function getMapWidth(): int
+            {
+                return $this->mapWidth;
+            }
+
+            public function getMapHeight(): int
+            {
+                return $this->mapHeight;
+            }
+
+            public function getDificultyPercentage(): int
+            {
+                return $this->dificultyPercentage;
+            }
+        };
+
+        $coordinatesService = new class () implements CoordinatesServiceInterface {
+            public function getNonOccupiedPoint(Map $map): Position
+            {
+                return factory(Position::class)->make();
+            }
+        };
+
+        $service = new MapGenerator($coordinatesService);
+
+        $game = factory(Game::class)->make();
+        $game->save();
+
+        $map = $service->create($settings, $game);
 
         $this->assertEquals($settings->getMapWidth(), $map->width);
         $this->assertEquals($settings->getMapHeight(), $map->height);
